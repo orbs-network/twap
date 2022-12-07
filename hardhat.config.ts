f5043ef2cf712522bea849714abe75dc6bf546a9
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-12-07 14:12:16 +0200

    serve configs from ghpages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,11 +7,25 @@ import "@nomiclabs/hardhat-web3";
 import "@nomiclabs/hardhat-etherscan";
 import { hardhatDefaultConfig, hre } from "@defi.org/web3-candies/dist/hardhat";
 import _ from "lodash";
+import fs from "fs-extra";
 
 task("deploy").setAction(async () => {
   if (hre().network.config.chainId === hre().config.networks?.hardhat?.chainId) throw new Error("on hardhat network!");
   if (process.env.NETWORK!.toLowerCase() !== hre().network.name.toLowerCase())
     throw new Error(`different networks!, ${process.env.NETWORK} != ${hre().network.name}`);
+});
+
+task("github-pages").setAction(async () => {
+  const src = require("./src");
+  await fs.writeJson("./docs/configs.json", src.Configs, { spaces: 2 });
+  await fs.writeJson(
+    "./docs/chain-configs.json",
+    _.mapValues(src.ChainConfigs, (cc) => ({
+      ...cc,
+      integrations: _.filter(src.Configs, (c) => c.chainId === cc.chainId).map((c) => c.partner),
+    })),
+    { spaces: 2 }
+  );
 });
 
 export default _.merge(hardhatDefaultConfig(), {
