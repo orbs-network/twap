e02e43dec9e5b9b2b1a7c5446cf68784087c3ec7
DanielZlotin <danielzlotin@users.noreply.github.com>
2024-10-31 12:41:24 +0200

    deployTaker

--- deploy.zsh
+++ deploy.zsh
@@ -5,13 +5,14 @@ config=./configs.json
 
 trap chain EXIT
 
-chains=$(jq -r ".[].chainId" $config | sort -u | grep -v '324')
+chains=$(jq -r ".[].chainId" $config | sort -u | grep -v '324') # zkSync not supported, use foundry-zksync
 
 echo $chains | parallel --keep-order "
-    echo \"\n🔗🔗🔗🔗🔗🔗🔗🔗🔗🔗🔗🔗 {} 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀\n\";
+    echo \"\n🔗🔗🔗🔗🔗🔗🔗🔗🔗🔗🔗🔗 {} \$CHAIN_NAME 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀\n\";
     chain {};
     export TWAP=\$(jq -r 'map(select(.chainId == {})) | unique_by(.twapAddress) | .[].twapAddress' $config);
     export ADMIN=\$(a admin);
-    forge script DeployTaker --broadcast --verify \$([[ -n \$VERIFIER ]] && echo --verifier \$VERIFIER);
+
+    forge script Deploy --broadcast --verify \$([[ -n \$VERIFIER ]] && echo --verifier \$VERIFIER);
     "
 
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,19 +6,26 @@ import "forge-std/Script.sol";
 
 import {Taker, TWAP, IAllowed} from "src/periphery/Taker.sol";
 
-contract DeployTaker is Script {
+contract Deploy is Script {
+    bytes32 constant SALT0 = 0x15f1c43aeb27f46f344846db8cd43cc355ceef71562c18b0e1e2b78b7f1a21bf;
+    bytes32 constant SALT1 = 0xdb8cb4d00d8df88cc17593fcdf17ac1ba5e363160bcee3824e0b77886bcfe657;
+
     error AddressMismatch(string message);
 
-    function run() public returns (address taker) {
+    function run() public returns (address taker, address taker2) {
         TWAP twap = TWAP(payable(vm.envAddress("TWAP")));
         IAllowed admin = IAllowed(vm.envAddress("ADMIN"));
+        taker = deployTaker(twap, admin, SALT0);
+        taker2 = deployTaker(twap, admin, SALT1);
+    }
 
+    function deployTaker(TWAP twap, IAllowed admin, bytes32 salt) private returns (address taker) {
         bytes32 initCodeHash = hashInitCode(type(Taker).creationCode, abi.encode());
-        taker = vm.computeCreate2Address(0, initCodeHash);
+        taker = vm.computeCreate2Address(salt, initCodeHash);
 
         if (taker.code.length == 0) {
             vm.broadcast();
-            Taker deployed = new Taker{salt: 0}();
+            Taker deployed = new Taker{salt: salt}();
             if (taker != address(deployed)) revert AddressMismatch("deployment");
 
             vm.broadcast();
