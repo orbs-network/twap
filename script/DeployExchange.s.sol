66e3ea4fb28be77411a85f4b6e43ab9babd0bf59
DanielZlotin <danielzlotin@users.noreply.github.com>
2025-01-26 17:23:07 +0200

    teafi

--- script/DeployExchange.s.sol
+++ script/DeployExchange.s.sol
@@ -6,11 +6,12 @@ import "forge-std/Script.sol";
 
 import {IExchange} from "src/IExchange.sol";
 import {RouterExchange} from "src/exchange/RouterExchange.sol";
+import {ParaswapExchange} from "src/exchange/ParaswapExchange.sol";
 
 contract DeployExchange is Script {
     function run() public returns (IExchange) {
         address router = vm.envAddress("ROUTER");
         vm.broadcast();
-        return new RouterExchange{salt: 0}(router);
+        return new ParaswapExchange{salt: 0}(router);
     }
 }
